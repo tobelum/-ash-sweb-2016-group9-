ed6c1ef48132ca5215d01ea087c0095ed1c60365
cliCode <jesse.akosa@gmail.com>
2016-03-18 12:38:45 +0000

    edit column name
    column groups changed to group_name

--- addPatient.php
+++ addPatient.php
@@ -9,7 +9,7 @@ function addPatient () {
 
 function newPatient ($patientID ='none',$username='none',$firstname='none',$lastname='none',$gender='none',$nationality='none', $insuranceType='none',$dob='none',$group=0,$phoneNumber='none'){
     
-    $strQuery = "insert into patients values patient_id = '$patientID',
+    $strQuery = "insert into patients SET patient_id = '$patientID',
                                            username = '$username',
                                            firstname = '$firstname',
                                            lastname = '$lastname',
@@ -17,7 +17,7 @@ function newPatient ($patientID ='none',$username='none',$firstname='none',$last
                                            nationality = '$nationality',
                                            insurance_type = '$insuranceType',
                                            dob = '$dob',
-                                           groups = '$group',
+                                           group_name = '$group',
                                            phone_number = '$phoneNumber'
                                     
                                            ";
@@ -27,7 +27,6 @@ return $this->query ($strQuery);
 }
 
 }
-
 /*$obj = new addPatient ();
 
 $result = $obj->addPatient('60702017','jesse.akosa','jesse','akosa','male','ghanaian','nhis',1993-03-02,1,'0279507667');
@@ -40,5 +39,6 @@ else {
 echo "add successful";
 }
 */
+
 ?>
 
