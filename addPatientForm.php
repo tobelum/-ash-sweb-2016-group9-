5babbf8fedde82b6a60379f0ea003d249f0a32b0
cliCode <jesse.akosa@gmail.com>
2016-03-18 16:37:28 +0000

    updated html

--- addPatientForm.php
+++ addPatientForm.php
@@ -1,35 +1,53 @@
-<html>
-	<head>
-		<title>Add New Patient</title>
-		<link rel="stylesheet" href="css/style.css">
-		<script>
-			<!--add validation js script here
-		</script>
-	</head>
-	<body>
-		<table>
-			<tr>
-				<td colspan="2" id="pageheader">
-					Add New Patient
-				</td>
-			</tr>
-			<tr>
-				<td id="mainnav">
-					<div class="menuitem">menu 1</div>
-					<div class="menuitem">menu 2</div>
-				</td>
-				<td id="content">
-					<div id="divPageMenu">
-						<span class="menuitem" >page menu 1</span>
-						<input type="text" id="txtSearch" />
-						<span class="menuitem">search</span>		
-					</div>
+		<! doctype html>
+		<html>
+			<head>
+				<title>Ashesi Health Center</title>
+				<link rel="stylesheet" href="css/style.css">
+				<script>
+					<!--add validation js script here
+				</script>
+			</head>
+			<body>
+				<table>
+					<tr>
+						<td colspan="2" id="pageheader">
+						<div id="div7"> <img src="C:\xampp\htdocs\Codes\code sample for practical\New folder\logo.jpg" height="80"/> </div>
+							Ashesi Health Center
+						</td>
+					</tr>
+					<tr>
+						<td id="mainnav">
+							<div class="menuitem"><a href="http://mail.office365.com/"> Send us an email </a></div>
+							<div class="menuitem"><a href="http://www.ashesi.edu.gh/student-life-5/health-and-wellbeing.html">
+								More info</a></div>
+							<div class="menuitem"><a href="http://www.who.int/topics/en/"> Health News</a></div>
+						</td>
+						<td id="mainnav1">
+							<!--Home Page-->
+							<div id="divPageMenu">
+								<span class="menuitem1" >Welcome to Ashesi Clinic Home Page</span>		
+							</div>
+							<div id="divContent">
+							<!--Links to the rest of the functional requirements -->
+							<div id="div2"> <a href="loginPage.php"> <h2 >Add New User</h2> </a> </div> 
+							<div id="div3"> <a href="loginPage.php"> <h2>Edit User</h2> </a> </div> 
+							<div id="div4"> <a href="loginPage.php"> <h2> Update</h2> </a> </div> 
+							<div id="div5"> <a href="searchpage.php"> <h2>view Patient Record: </h2> </a> </div>
+							</div>
+						</td>
+						
+					</tr>
+                    		
+	
+				</table>
+			</body>
+
+		</html>	
+						
 					
-				</td>
-			</tr>
-		</table>
-	</body>
-</html>	
+
+
+	
 
 
 
@@ -96,6 +114,22 @@ input[type=text] { float: left; width: 250px; }
                            
             </body>
         </html>
+<html>
+<tr>
+						<td colspan="2" id="pagefooter">
+						<!--This is the footer page -->
+						<footer>
+								<p>©Ashesi University College. All rights reserved.</p>
+								<p>1 University Avenue, Berekuso; PMB CT 3, Cantonments, Accra, Ghana | Phone: +233.302.610.330</p>
+						 </footer>
+						</td>
+					</tr>	
+					
+				</table>
+			</body>
+
+		</html>	
+
                         
     <?php
     include_once("addPatient.php");
