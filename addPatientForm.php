a467cc0be4fb68bea351dfc29372d5e2763038e0
cliCode <jesse.akosa@gmail.com>
2016-03-18 13:57:50 +0000

    HTML and CSS
    Align text boxes and create temporal background

--- addPatientForm.php
+++ addPatientForm.php
@@ -1,23 +1,72 @@
-    <html>
+<html>
+	<head>
+		<title>Add New Patient</title>
+		<link rel="stylesheet" href="css/style.css">
+		<script>
+			<!--add validation js script here
+		</script>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td colspan="2" id="pageheader">
+					Add New Patient
+				</td>
+			</tr>
+			<tr>
+				<td id="mainnav">
+					<div class="menuitem">menu 1</div>
+					<div class="menuitem">menu 2</div>
+				</td>
+				<td id="content">
+					<div id="divPageMenu">
+						<span class="menuitem" >page menu 1</span>
+						<input type="text" id="txtSearch" />
+						<span class="menuitem">search</span>		
+					</div>
+					
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>	
+
+
+
+       <html>
         <head>
-            
+           <style>
+
+form { width: 400px; }
+
+label { float: left; width: 150px; }
+
+input[type=text] { float: left; width: 250px; }
+
+.clear { clear: both; height: 0; line-height: 0; }
+
+.floatright { float: right; }
+
+</style>
+ 
         </head>
         <body>
+            <br/>
             
             <table>
             <tr>
                 <td>
-            <form action="addPatientForm.php" method="GET" align = middle>
+            <form action="addPatientForm.php" method="GET" >
 
-                <div>Patient ID: <input type="text" name="patientID" </div><br></br>
-                    <div>User Name: <input text-align="right" type="text" name="username" </div><br></br>
-                    <div>First Name: <input type="text" name="firstname" </div><br></br>
-                    <div>Last Name: <input type="text" name="lastname" </div> <br></br>
-                    <div>Gender: <input type="text" name="gender" </div><br></br>
-                    <div>Nationality: <input type="text" name="nationality" </div><br></br>
-                    <div>Insurance Type: <input type="text" name="insuranceType" </div><br></br>
-                    <div>Date Of Birth: <input type="text" name="dob" </div> <br></br>
-                     <div>Group Type: <select name="patientgroup">
+                    <label for="patientID">Patient ID:</label> <input type="text" name="patientID" /><br></br>
+                    <label for="username">User Name:</label> <input type="text" name="username" /><br></br>
+                    <label for="firstname">First Name:</label> <input type="text" name="firstname" /><br></br>
+                    <label for="lastname">Last Name:</label> <input type="text" name="lastname"/><br></br>
+                    <label for="gender">Gender:</label><input type="text" name="gender" /><br></br>
+                    <label for="nationality">Nationality:</label> <input type="text" name="nationality" /><br></br>
+                    <label for="insuranceType">Insurance Type:</label> <input type="text" name="insuranceType"/> <br></br>
+                    <label for="dob">Date of Birth:</label> <input type="text" name="dob"/> <br></br>
+                    <label for="patientgroup">Patient Group:</label> <select name="patientgroup">
     <?php
         //a call to the class
         include_once("patientGroups.php");
@@ -37,25 +86,25 @@
 
     ?>				
                             </select>
-                            </div><br></br>
-                    <div>Phone Number: <input type="text" name="phoneNum" </div><br></br>
-                        <div>Email: <input type="text" name="email" </div> <br></br>
+                            <br></br>
+                        <label for="phoneNum">Phone Number:</label> <input type="text" name="phoneNum"/><br></br>
+                        <label for="email">Email:</label> <input type="text" name="email" /><br></br>
 
-
+                        <br class="clear" /> <br />
                             <input type="submit" value="Add">
                 </form>	
                            
             </body>
         </html>
-        
-         <?php
+                        
+    <?php
     include_once("addPatient.php");
     
 	if(!isset($_REQUEST['patientID'])){
 		exit();		
 	}
     $obj= new addPatient();
-	print_r($_REQUEST);
+	//print_r($_REQUEST);
     $patientID=$_REQUEST['patientID'];
 	$username=$_REQUEST['username'];
 	$firstname=$_REQUEST['firstname'];
@@ -79,4 +128,6 @@
 	
 	
 ?>
+
+
 
