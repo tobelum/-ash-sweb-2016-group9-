acdf771186c9cb0e0192eaa4351c043e45a78b8b
cliCode <jesse.akosa@gmail.com>
2016-03-18 12:42:44 +0000

    the html form
    this will interact with the client

---
+++ addPatientForm.php
@@ -0,0 +1,28 @@
+    <html>
+        <head>
+            
+        </head>
+        <body>
+            
+            <table>
+            <tr>
+                <td>
+            <form action="addPatientForm.php" method="GET" align = middle>
+
+                <div>Patient ID: <input type="text" name="patientID" </div><br></br>
+                    <div>User Name: <input text-align="right" type="text" name="username" </div><br></br>
+                    <div>First Name: <input type="text" name="firstname" </div><br></br>
+                    <div>Last Name: <input type="text" name="lastname" </div> <br></br>
+                    <div>Gender: <input type="text" name="gender" </div><br></br>
+                    <div>Nationality: <input type="text" name="nationality" </div><br></br>
+                    <div>Insurance Type: <input type="text" name="insuranceType" </div><br></br>
+                    <div>Date Of Birth: <input type="text" name="dob" </div> <br></br>
+<div>Phone Number: <input type="text" name="phoneNum" </div><br></br>
+                        <div>Email: <input type="text" name="email" </div> <br></br>
+
+
+                            <input type="submit" value="Add">
+                </form>	
+                           
+            </body>
+        </html>
