4dbf800c38b0bdb30b3342e5b24eb673cff9d2b7
RoslynneAmoah <roslynne.amoah@ashesi.edu.gh>
2016-03-21 20:39:56 +0000

    Update patientGroup.php

--- groupwork/patientGroup.php
+++ groupwork/patientGroup.php
@@ -9,19 +9,7 @@ class patientGroup extends adb{
 		$strQuery="select groupID, groupName from patientgroup";
 		return $this->query($strQuery);
 	
-	}
-/*	 function addPatientGroup($text){
-	 	$strQuery="insert into patientgroup (groupName) values ('$text')";
-	 	return $this->query($strQuery);
-	 	
-	 	if (!$this->query($strQuery)){
-			echo "Error adding PATIENTgroup";
-			exit();
-	 	}else{
-	 		echo "Success adding usergroup";
-	 	}
-	 }
-	 */
+}
 
 }
 
