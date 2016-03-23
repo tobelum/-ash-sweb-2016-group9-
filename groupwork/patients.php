d3f94d04d989c2a108ef76bbb6548c167a3f3d47
RoslynneAmoah <roslynne.amoah@ashesi.edu.gh>
2016-03-23 23:53:28 +0000

    Update patients.php

--- groupwork/patients.php
+++ groupwork/patients.php
@@ -16,7 +16,7 @@
 	*@param string email email address
 	*/
 
-     class patients extends adb{
+         class patients extends adb{
 
 /**  
      *Function search patients
@@ -32,7 +32,7 @@
                return $this->getPatients($filter);
           }
 // select username,firstname,lastname,gender,nationality,insurance_type,dob,group_name,phone_number,
-//             email from patients left join patientgroup on patients.group_name=patientgroup.groupID where patient_id = '84082017'
+//             email from patients left join patientgroup on patients.group_name=patientgroup.groupID where patient_id = '84082017';
 
 /**  
      *Function get patients
@@ -40,7 +40,7 @@
 */
      	function getPatients($filter=false){
 
-     	  $strQuery = "select patient_id, username,firstname,lastname,gender,nationality,insurance_type,dob,group_name,phone_number,
+     	  $strQuery = "select patient_id, username,firstname,lastname,gender,nationality,insurance_type,dob,patientgroup.groupName,phone_number,
             email from patients left join patientgroup on patients.group_name=patientgroup.groupID"; 
                if($filter!=false){
                     $strQuery=$strQuery . " where $filter";
@@ -49,21 +49,23 @@
                return $this->query($strQuery);
      	}
 
+// $obj = new patients();
+//getPatients('84082017','roslynne.amoah','roslynne','amoah','female','Nigerian','MED-X','1994-02-17','0','208497123','oslynne.amoah@ashesi.edu.gh');
+
+
 /**  
      *Function edit Patient
      *@param int $filter contains $patientID,$username,$firstname,$lastname,$gender,$nationality,$insurance_type,
      *       $dob,$group,$phone_number,$email which allow the user to change these details the application.
 */
-
           function editPatient($patientID,$username,$firstname,$lastname,$gender,$nationality,$insurance_type,
-               $dob,$group,$phone_number,$email){
+               $dob,$group_name,$phone_number,$email){
 
                $strQuery="update patients set username='$username',firstname='$firstname',lastname='$lastname',
-               gender='$gender',nationality='$nationality',insurance_type='$insurance_type',dob='$dob',group_name='$group',
+               gender='$gender',nationality='$nationality',insurance_type='$insurance_type',dob='$dob',group_name='$group_name',
                phone_number='$phone_number',email='$email' where patient_id='$patientID'";
 
                return $this->query($strQuery);
           }
-
 
      }
