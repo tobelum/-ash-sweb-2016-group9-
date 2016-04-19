fd1f5d4a42a1bd1d64da43d1eeb2c84addec8b04
cliCode <jesse.akosa@gmail.com>
2016-04-19 23:13:23 +0000

    Modified
    New css file to crfeate a new background

--- addPatientForm.php
+++ addPatientForm.php
@@ -1,59 +1,13 @@
 		<! doctype html>
 		<html>
-			<head>
-				<title>Ashesi Health Center</title>
-				<link rel="stylesheet" href="css/style.css">
-				<script>
-					<!--add validation js script here
-				</script>
-			</head>
-			<body>
-				<table>
-					<tr>
-						<td colspan="2" id="pageheader">
-						<div id="div7"> <img src="C:\xampp\htdocs\Codes\code sample for practical\New folder\logo.jpg" height="80"/> </div>
-							Ashesi Health Center
-						</td>
-					</tr>
-					<tr>
-						<td id="mainnav">
-							<div class="menuitem"><a href="http://mail.office365.com/"> Send us an email </a></div>
-							<div class="menuitem"><a href="http://www.ashesi.edu.gh/student-life-5/health-and-wellbeing.html">
-								More info</a></div>
-							<div class="menuitem"><a href="http://www.who.int/topics/en/"> Health News</a></div>
-						</td>
-						<td id="mainnav1">
-							<!--Home Page-->
-							<div id="divPageMenu">
-								<span class="menuitem1" >Welcome to Ashesi Clinic Home Page</span>		
-							</div>
-							<div id="divContent">
-							<!--Links to the rest of the functional requirements -->
-							<div id="div2"> <a href="loginPage.php"> <h2 >Add New User</h2> </a> </div> 
-							<div id="div3"> <a href="loginPage.php"> <h2>Edit User</h2> </a> </div> 
-							<div id="div4"> <a href="loginPage.php"> <h2> Update</h2> </a> </div> 
-							<div id="div5"> <a href="searchpage.php"> <h2>view Patient Record: </h2> </a> </div>
-							</div>
-						</td>
-						
-					</tr>
-                    		
-	
-				</table>
-			</body>
-
-		</html>	
-						
-					
-
-
-	
-
-
-
-       <html>
-        <head>
-           <style>
+	<head>
+		<title>Ashesi Health Center: Update Patient's Information</title>
+		<link rel="stylesheet" href="css/styles.css">
+		<script>
+			<!--add validation js script here
+		</script>
+		
+		 <style>
 
 form { width: 400px; }
 
@@ -66,9 +20,35 @@ input[type=text] { float: left; width: 250px; }
 .floatright { float: right; }
 
 </style>
- 
-        </head>
-        <body>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td colspan="2" id="pageheader">
+				<div id="div7"> <img src="logo.png" height="60"/> </div>
+					<font color="white">Ashesi Health Center</font>
+				<ul>
+			  <li><a href="">Home</a></li>
+			  <li><a href="">Edit Patient</a></li>
+			  <li><a href="">View Patient</a></li>
+			  <li><a href="">Add New Patient</a></li>
+			  <li><a href="">Add New Diagnosis</a></li>
+		      <li2 ><a href="logout.php"><font color = 'white'>Logout</font> </a></li2>
+			</ul>
+				</td>
+
+			</tr>
+			<tr>
+				<!--Links to important websites-->
+				
+				<td id="content">
+						<div id="divPageMenu">
+							<span class="menuitem1" >Add New Patient</span>		
+						</div>
+						
+				
+        
+		<div id="divContent">
             <br/>
             
             <table>
@@ -110,23 +90,31 @@ input[type=text] { float: left; width: 250px; }
 
                         <br class="clear" /> <br />
                             <input type="submit" value="Add">
+                       
                 </form>	
-                           
-            </body>
-        </html>
-<html>
+				 </table>  
+                    </div>       
+           
+
 <tr>
-						<td colspan="2" id="pagefooter">
-						<!--This is the footer page -->
-						<footer>
-								<p>©Ashesi University College. All rights reserved.</p>
-								<p>1 University Avenue, Berekuso; PMB CT 3, Cantonments, Accra, Ghana | Phone: +233.302.610.330</p>
-						 </footer>
-						</td>
-					</tr>	
+				<td colspan="2" id="pagefooter">
+				<footer>
+
+					<div class="menuitem"><a href="http://mail.office365.com/"><font color = 'white'> Send us an email </font></a></div>
+					<div class="menuitem"><a href="http://www.ashesi.edu.gh/student-life-5/health-and-wellbeing.html"><font color = 'white'>
+						More Info</font></a></div>
+					<div class="menuitem"><a href="http://www.who.int/topics/en/"><font color = 'white'> Health News</font></a></div>
+			
+						<?php echo '<a href="logout.php">Logout </a>'; ?>
+						<p>©Ashesi University College. All rights reserved.</p>
+						<p>1 University Avenue, Berekuso; PMB CT 3, Cantonments, Accra, Ghana | Phone: +233.302.610.330</p>
+				 </footer>
+				</td>
+			</tr>	
+							
 					
 				</table>
-			</body>
+			 </body>
 
 		</html>	
 
@@ -135,6 +123,7 @@ input[type=text] { float: left; width: 250px; }
     include_once("addPatient.php");
     
 	if(!isset($_REQUEST['patientID'])){
+        echo "No data inputed";
 		exit();		
 	}
     $obj= new addPatient();
