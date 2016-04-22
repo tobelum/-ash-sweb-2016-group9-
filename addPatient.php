9470441eca2c13f22d6a82cc925ec1c5aab1b91e
cliCode <jesse.akosa@gmail.com>
2016-04-22 13:20:36 +0000

    SQL edit
    Query was modified due to errors

--- addPatient.php
+++ addPatient.php
@@ -7,20 +7,10 @@ function addPatient () {
 }
     
 
-function newPatient ($patientID ='none',$username='none',$firstname='none',$lastname='none',$gender='none',$nationality='none', $insuranceType='none',$dob='none',$group=0,$phoneNumber='none'){
+function newPatient ($patientID ='none',$username='none',$firstname='none',$lastname='none',$gender='none',$nationality='none', $insuranceType='none',$dob='none',$group=0,$phoneNumber='none',$email='none'){
     
-    $strQuery = "insert into patients SET patient_id = '$patientID',
-                                           username = '$username',
-                                           firstname = '$firstname',
-                                           lastname = '$lastname',
-                                           gender = '$gender',
-                                           nationality = '$nationality',
-                                           insurance_type = '$insuranceType',
-                                           dob = '$dob',
-                                           group_name = '$group',
-                                           phone_number = '$phoneNumber'
-                                    
-                                           ";
+    $strQuery = "INSERT INTO `webtech`.`patients` (`patient_id`, `username`, `firstname`, `lastname`, `gender`, `nationality`, `insurance_type`, `dob`, `group_name`, `phone_number`, `email`) VALUES ('$patientID', '$username', '$firstname', '$lastname', '$gender', '$nationality', '$insuranceType', '$dob', '$group', '$phoneNumber', '$email')";
+                                           
 return $this->query ($strQuery);
 
 
@@ -29,7 +19,7 @@ return $this->query ($strQuery);
 }
 /*$obj = new addPatient ();
 
-$result = $obj->addPatient('60702017','jesse.akosa','jesse','akosa','male','ghanaian','nhis',1993-03-02,1,'0279507667');
+$result = $obj->newPatient('60702017','jesse.akosa','jesse','akosa','male','ghanaian','nhis',1993-03-02,1,'0279507667');
 
 if ($result = false){
 echo "add unsuccessful";
