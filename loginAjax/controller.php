225346a35a404d1b56f8b2092fc1fb88f0374939
Francis Yinbil <francis.yinbil@ashesi.edu.gh>
2016-04-26 02:46:23 +0000

    created the login function for the server side

--- loginAjax/controller.php
+++ loginAjax/controller.php
@@ -5,5 +5,74 @@
 
 <?php 
 
+	/**
+		*Start a session before proceding
+	*/
+		session_start();
 
+		if(!isset($_REQUEST['cmd'])){
+		  echo '{"result": 0, "message": "Unknown command"}';
+		  return;
+		}
+	/**
+		*Check command
+		*@param cmd
+	*/
+		$cmd = $_REQUEST['cmd'];
+
+		switch ($cmd) {
+		  case 1:
+		  Login(); // call the login functio
+		  break;
+
+		  default:
+		  echo '{"result": 0, "message": "Unknown command"}';
+		  return;
+		  break;
+		}
+
+	/**
+		*Login function that enables you login with your username and password
+		*@return boolean ture if connected else false
+	*/
+		function Login(){
+			
+			//check if there is a user code
+				if(!isset($_REQUEST['username'])&& !isset($_REQUEST['password'])){
+					echo "username & password not given";	//change to json message	
+					exit();
+				}
+		  // include the users file		
+		  include "users.php";
+
+		  $myuser = new users();
+
+		  $username = $_GET['username'];
+		  $password = $_GET['password'];
+		  $password_hash= MD5($password); // password is encrypted
+		  $myuser->Login($username, $password_hash);
+		  $row=$myuser->fetch();
+
+		  if($row){
+			session_destroy(); 
+			session_start(); // start session here
+
+			$_SESSION['username'] = $username;
+			echo '{"result": 1, "user": [';
+			while($row){
+			  echo json_encode($row);
+			  $row = $myuser->fetch();
+			  if($row){
+				echo ',';
+			  }
+			}
+			echo "]}";
+			return; 
+		  }
+		  echo '{"result": 0, "message": "Wrong details! Please try again"}';
+		  return;
+
+		}
+
+	
 ?>
