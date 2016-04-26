705549f74e14d6d27baa23054fbf2cfd6e52f8c3
RoslynneAmoah <roslynne.amoah@ashesi.edu.gh>
2016-04-26 14:48:43 +0000

    Update editAjax.php
    Added groupID and group_name to getPatients()

--- editAjax.php
+++ editAjax.php
@@ -1,64 +1,68 @@
 <?php
-	//check command
-	if (!isset($_REQUEST['command'])) {
-		echo "command has not been provided";
-		return;
-	}
+     include_once("adb.php");
+     
+     /**
+	*Variables used in all the functions for Patients
+	*@param int patient_id patient ID
+	*@param string username username
+	*@param string firstname first name
+	*@param string lastname last name
+	*@param string gender gender
+	*@param string nationality nationality
+	*@param string insurance_type insurance type
+	*@param date dob date of birth
+	*@param int group_name group name
+	*@param int phone_number phone number
+	*@param string email email address
+	*/
 
-	/*get command*/
-	$command = $_REQUEST['command'];
-	switch($command) {
-		case 1:
-			editPatient(); 			//if command=1 update Patient's info
-			break;
-		default:
-			echo "wrong command";	//change to json message
-			break;
-	}
+         class patients extends adb{
 
-	function editPatient () {
-		// check if there is a patients ID
-		if (!isset($_REQUEST['patientID'])) {
-			echo '{"result": 0 "message": "Patient not set"}';
-			//echo "0";
-			exit();
-		 }
-
-
-		 // check if each input has been placed into textbox
-		 if((!isset($_REQUEST['patientID']))&&(!isset($_REQUEST['username']))&&(!isset($_REQUEST['firstname']))&&(!isset($_REQUEST['lastname']))
-		 	&&(!isset($_REQUEST['gender']))&&(!isset($_REQUEST['nationality']))&&(!isset($_REQUEST['insurance_type']))&&(!isset($_REQUEST['dob']))
-		 	&&(!isset($_REQUEST['phone_number']))&&(!isset($_REQUEST['email']))&&(!isset($_REQUEST['group_name'])))
-		 	echo '{"result": 0 "message": "The information needed to update has not been provided"}';
-
-		 		//Store variables for the URL
-		  		$patientID = $_REQUEST['patientID'];
-		  		$username=$_REQUEST['username'];
-		  		$firstname=$_REQUEST['firstname'];
-		  		$lastname=$_REQUEST['lastname'];
-		 	    $gender=$_REQUEST['gender'];
-		  		$nationality=$_REQUEST['nationality'];
-		 	 	$insurance_type=$_REQUEST['insurance_type'];
-		  		$dob=$_REQUEST['dob'];
-		  		$phone_number=$_REQUEST['phone_number'];
-		  		$email=$_REQUEST['email'];
-		  		$group_name=$_REQUEST['group_name'];
-			
-			//Create an object of the patients class
-			include_once("patients.php");
-			$patient = new patients();
-			
-			//update patients information
-			$edit = $patient->editPatient($patientID,$username,$firstname,$lastname,$gender,$nationality,$insurance_type,
-										  $dob,$group_name,$phone_number,$email);
-
-			//check whether the edit function from the patient's class is working
-			if (!$edit) {
-				echo '{"result": 0, "message": "Error Updating Patient\'s information"}';
-				return;
+/**  
+     *Function search patients
+     *@param int $filter contains the Patient ID
+*/
+		function searchPatients($text=false){
+			$filter=false;
+			if($text!=false){
+				$filter=" patient_ID like '%$text%' or username like '%$text%' or firstname like '%$text%' or lastname 
+				like '%$text%' or gender like '$text' or nationality like '%$text%' or insurance_type like '%$text%'
+                    ";
 			}
-			else {
-				echo '{"result": 1, "message": "Patient\'s information sucessfully updated"}';
-			}				
-	}
-?>
+			
+			return $this->getPatients($filter);
+			
+		}
+
+/**  
+     *Function get patients
+     *@param int $filter contains the Patient ID abd retrieves the information from the database
+*/
+     	function getPatients($filter=false){
+
+     	  $strQuery = "select patient_id, username,firstname,lastname,gender,nationality,insurance_type,dob,patientgroup.groupName,phone_number,groupID,group_name,
+            email from patients left join patientgroup on patients.group_name=patientgroup.groupID"; 
+               if($filter!=false){
+                    $strQuery=$strQuery . " where $filter";
+
+          }
+          
+               return $this->query($strQuery);
+     	}
+
+/**  
+     *Function edit Patient
+     *@param int $filter contains $patientID,$username,$firstname,$lastname,$gender,$nationality,$insurance_type,
+     *       $dob,$group,$phone_number,$email which allow the user to change these details the application.
+*/
+          function editPatient($patientID,$username,$firstname,$lastname,$gender,$nationality,$insurance_type,
+               $dob,$group_name,$phone_number,$email){
+
+               $strQuery="update patients set username='$username',firstname='$firstname',lastname='$lastname',
+               gender='$gender',nationality='$nationality',insurance_type='$insurance_type',dob='$dob',group_name='$group_name',
+               phone_number='$phone_number',email='$email' where patient_id='$patientID'";
+
+               return $this->query($strQuery);
+          }
+
+     }
